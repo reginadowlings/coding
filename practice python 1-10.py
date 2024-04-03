35a0c04b2a029ec96f99a3f4057c3674d7bd357b
Regina-Dowlings8 <reginadowlings@gmail.com>
2024-04-03 13:39:10 +0000

    Python Practice

--- practice python 1-10.py
+++ practice python 1-10.py
@@ -12,12 +12,12 @@ print(user_name, 'you will trun 100 in the year', year_when_turned_cen_old )
 
 #  Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user
 
-# num = int(input('give me a number: '))
+num = int(input('give me a number: '))
 
-# if num % 2 ==0:
-#     print('You entered an even number')
-# else:
-#     print('That is odd!')
+if num % 2 ==0:
+    print('You entered an even number')
+else:
+    print('That is odd!')
 
 
 # write a program that prints out all the elements of the list that are less than 5.
@@ -44,8 +44,8 @@ for i in range(1, user_number+1):
 # write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
 
-''
-'''def common_numbers(list1, list2): 
+
+def common_numbers(list1, list2): 
     result = []
 
     set_1 = set(list1) 
@@ -62,32 +62,32 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 same = common_numbers(a, b)
 print(same)''
-'''
+
 
 # write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your 
 # program works on two lists of different sizes. Write this in one line of Python 
-'''
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 same = set([element for element in a if element in b])
-print(list(same))'''
+print(list(same))
 
 
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards 
 # and backwards.)
 
-'''string = input('give me a word: ')
+string = input('give me a word: ')
 string_backwards = string[:: -1]
 
 if string == string_backwards:
     print('The word', string, 'is a palindrome ')
 else:
-    print('Your word', string, 'isn\'t a palindrome')'''
+    print('Your word', string, 'isn\'t a palindrome')
 
 
 # Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to
 #  the winner, and ask if the players want to start a new game)
-'''import random 
+import random 
 
 def rps_game():
 
@@ -123,11 +123,11 @@ def rps_game():
         print('You didn\'t enter a valid choice')
 
 rps_game()
-'''
+
 
 # Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low,
 # too high, or exactly right. 
-'''import random
+import random
 
 random_guess = random.randint(1,9)
 
@@ -141,7 +141,7 @@ elif user_guess < random_guess:
 elif user_guess > random_guess:
     print('your guess is too high')
 else:
-    print('no correct input entered')'''
+    print('no correct input entered')
 
 
 
--- pratice python 11 -20.py
+++ pratice python 11 -20.py
@@ -1,7 +1,7 @@
 # Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no 
 # divisors.).
 
-'''def is_prime(number):
+def is_prime(number):
     if number < 2:
         return False
     for i in range(2, int(number**0.5) + 1):
@@ -16,22 +16,22 @@ number = int(input("Enter a number: "))
 if is_prime(number):
     print(number, "is a prime number.")
 else:
-    print(number, "is not a prime number.")'''
+    print(number, "is not a prime number.")
 
 
 # Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of
 # the given list. For practice, write this code inside a function.
 
-# def bend(numbers):
-#     if not numbers:
-#         return []
-#     return [numbers[0], numbers[-1]]
+def bend(numbers):
+    if not numbers:
+        return []
+    return [numbers[0], numbers[-1]]
 
-# a = [5, 10, 15, 20, 25]
-# first_last = bend(a)
-# print(list(first_last))
+a = [5, 10, 15, 20, 25]
+first_last = bend(a)
+print(list(first_last))
 
-'''RETURNING THE FIRST INDEX'''
+# RETURNING THE FIRST INDEX
 
 def first_number(numbers):
     for num in numbers:
@@ -61,7 +61,7 @@ print(new_age_list)
 
 
 # USING FOR LOOP
-'''
+
 def get_unduplicated_list(list_of_numbers):
     new_list = []
     for num in list_of_numbers:
@@ -72,25 +72,25 @@ def get_unduplicated_list(list_of_numbers):
     
 ages = [1,4,5,2,1,4,6,4,9]
 new_age_list = get_unduplicated_list(ages)
-print(new_age_list)'''
+print(new_age_list)
 
 
 # Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, 
 # except with the words in backwards order. 
     
-# def reverse_string(multiple_strings):
-#     splitting = multiple_strings.split()
-#     reversed_strings = ''.join(reversed(splitting))
-#     return  reversed_strings
+def reverse_string(multiple_strings):
+    splitting = multiple_strings.split()
+    reversed_strings = ''.join(reversed(splitting))
+    return  reversed_strings
 
-# input_strings = input('Make a sentence of 5 words: ')
-# reversing = reverse_string(input_strings)
-# print(reversing)
+input_strings = input('Make a sentence of 5 words: ')
+reversing = reverse_string(input_strings)
+print(reversing)
 
 
-# name = 'regina'
-# rev = name[::-1]
-# print(rev)
+name = 'regina'
+rev = name[::-1]
+print(rev)
 
 
 # Write a password generator in Python. Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase 
@@ -98,45 +98,45 @@ print(new_age_list)'''
 # Include your run-time code in a main method.
 
 
-# import random
-# characters = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm!@#$%^*'
+import random
+characters = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm!@#$%^*'
 
-# while True:
-#     user_prompt = input("Enter 'generate' to get password or 'exit' to quit: ")
-#     if user_prompt == 'exit':
-#         break
-#     elif user_prompt == 'generate':
-#         length = random.randint(10,15)
-#         password = ''.join(random.choices(characters,  k = length))
-#         print(password)
-#     else:
-#         print('No prompt entered')
+while True:
+    user_prompt = input("Enter 'generate' to get password or 'exit' to quit: ")
+    if user_prompt == 'exit':
+        break
+    elif user_prompt == 'generate':
+        length = random.randint(10,15)
+        password = ''.join(random.choices(characters,  k = length))
+        print(password)
+    else:
+        print('No prompt entered')
 
 
 
-# name = input('enter your name: ')
+name = input('enter your name: ')
 
-# while True:
-#     if name == '':
-#         print('you did not enter your name')
-#         name = input('enter your name: ')
-#     else:
-#         print(f'annyeong, {name}')
-#         break
+while True:
+    if name == '':
+        print('you did not enter your name')
+        name = input('enter your name: ')
+    else:
+        print(f'annyeong, {name}')
+        break
 
-# import random
-# import string 
-# length = random.randint(9,12)
+import random
+import string 
+length = random.randint(9,12)
 
-# ran_charaters = ''.join(random.choices(string.ascii_letters,  k = length))
-# print(ran_charaters)
+ran_charaters = ''.join(random.choices(string.ascii_letters,  k = length))
+print(ran_charaters)
 
 
 # Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number.
 # The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
 # number = []
 
-'''def list_of_strings(list, number):
+def list_of_strings(list, number):
     ordered_list = sorted(list)
     if number in ordered_list:
         return True
@@ -146,18 +146,18 @@ print(new_age_list)'''
 nums = [2,3,6,5,1,4]
 order = list_of_strings(nums, 7)
 print(order)
-    '''  
-
-# a = '---'.join('    ')
-# b = '   '.join('||||')
-# print('\n'.join((a, b, a, b, a, b, a)))
 
 
-# myTuple = ("John", "Peter", "Vicky")
+a = '---'.join('    ')
+b = '   '.join('||||')
+print('\n'.join((a, b, a, b, a, b, a)))
 
-# x = "#".join(myTuple)
 
-# print(x)
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print(x)
 
 
 # You, the user, will have in your head a number between 0 and 100. The program will guess a number, and you, the user, will say whether it is too 
